18aad7ed34d4fc8391505024b86438d47fe221e1
AnonymousNoman <>
2021-04-04 02:09:53 +0000

    repl buld

---
+++ buildNRun.sh
@@ -0,0 +1,3 @@
+cmake --build . --target DatabaseApp -- -j 9
+chmod +x out/Linux/Debug/DatabaseApp
+./out/Linux/Debug/DatabaseApp
---
+++ projectConfigure.sh
@@ -0,0 +1,3 @@
+rm -rf CMakeLists
+cmake  -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" .
+touch buildNRun.sh
